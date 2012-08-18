fcf58817148d5ac6a747c7149a3284204bc90eb5
Dima Kukushkin <dima@kukushkin.me>
2012-08-18 18:12:00 +0400

    Add select blog form

---
+++ apps/blogs/forms.py
@@ -0,0 +1,21 @@
+from blogs.utils import api_resource
+
+from django import forms
+from django.conf import settings
+
+
+class BlogForm(forms.Form):
+
+    blog = forms.ChoiceField(choices=())
+
+    def __init__(self, token, *args, **kwargs):
+        super(BlogForm, self).__init__(*args, **kwargs)
+        choices = ((None, 'Select your blog'),)
+        blogs_data = api_resource(
+            settings.BLOGGER_API_ROOT,
+            'users/self/blogs',
+            token
+        )
+        for blog in blogs_data['items']:
+            choices+=((blog['id'], blog['name']),)
+        self.fields['blog'].choices = choices
--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from blogs.forms import BlogForm
+
 from repositories.forms import RepoForm
 
+
 def home(request):
-    return render(request, 'index.html', {'repo_form': RepoForm})
+    blog_source = request.session.get('blog_source')
+    if blog_source and not blog_source.is_expired:
+        blog_form = BlogForm(token=blog_source.access_token)
+    else:
+        blog_form = None
+    return render(request, 'index.html', {
+        'repo_form': RepoForm, 'blog_form': blog_form
+    })
