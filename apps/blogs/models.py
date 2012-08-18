e017158fcae9aa8044b00ac0c04660c611475291
Dima Kukushkin <dima@kukushkin.me>
2012-08-18 15:24:37 +0400

    Add create_or_update manager method for blogs.Source, remove refresh_token

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,21 +1,39 @@
+from datetime import datetime
 from django.db import models
 
 
 SOURCES = (('blogger', 'Blogger'),)
 
 
+class SourceManager(models.Manager):
+
+    def create_or_update(self, **kwargs):
+        instance, created = self.get_or_create(**kwargs)
+        defaults = kwargs.get('defaults', {})
+        if not created:
+            for key, value in defaults.items():
+                setattr(instance, key, value)
+            instance.save()
+        return instance, created
+
+
 class Source(models.Model):
 
-    type = models.CharField(max_length=50, choices=SOURCES)
+    type = models.CharField(max_length=50, choices=SOURCES, default='blogger')
     user_id = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
 
     access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
     expiration_datetime = models.DateTimeField()
+
+    objects = SourceManager()
 
     def __unicode__(self):
         return u'{0} on {1}'.format(self.username, self.get_type_display())
+
+    @property
+    def is_expired(self):
+        return self.expiration_datetime < datetime.now()
 
 
 class Blog(models.Model):
