7c323f4f0ee6d080b645df3d84fbdd1ad35b3b15
Dima Kukushkin <dima@kukushkin.me>
2012-08-18 19:41:50 +0400

    Add blog saving

--- apps/blogs/forms.py
+++ apps/blogs/forms.py
@@ -3,20 +3,38 @@ from blogs.utils import api_resource
 from django import forms
 from django.conf import settings
 
+from .models import Blog
+
 
 class BlogForm(forms.Form):
 
     blog = forms.ChoiceField(choices=())
 
-    def __init__(self, token, *args, **kwargs):
+    def __init__(self, blog_source, *args, **kwargs):
         super(BlogForm, self).__init__(*args, **kwargs)
+        self.blog_source = blog_source
         choices = ((None, 'Select your blog'),)
         blogs_data = api_resource(
             settings.BLOGGER_API_ROOT,
             'users/self/blogs',
-            token
+            blog_source.access_token
         )
         if blogs_data:
             for blog in blogs_data['items']:
                 choices+=((blog['id'], blog['name']),)
         self.fields['blog'].choices = choices
+
+    def save(self):
+        blog_id = self.cleaned_data.get('blog')
+        blog_data = api_resource(
+            settings.BLOGGER_API_ROOT,
+            'blogs/{0}'.format(blog_id),
+            self.blog_source.access_token
+        )
+        if blog_data:
+            blog = Blog.objects.create(
+                source=self.blog_source, identificator=blog_id,
+                default_domain=blog_data['url'], domain=''
+            )
+            return blog
+        return None
--- project/core/views.py
+++ project/core/views.py
@@ -8,7 +8,7 @@ from repositories.forms import RepoForm
 def home(request):
     blog_source = request.session.get('blog_source')
     if blog_source and not blog_source.is_expired:
-        blog_form = BlogForm(token=blog_source.access_token)
+        blog_form = BlogForm(blog_source=blog_source)
     else:
         blog_form = None
     return render(request, 'index.html', {
