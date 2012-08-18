27486b3fd4fb1bdd5d0dcde254812ee7605b6959
Alexander Aslanov <trolev@gmail.com>
2012-08-18 19:54:12 +0400

    Add js for blog save

--- layout/media/js/script.js
+++ layout/media/js/script.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
       $('li:first', block).hide();
     }
   });
+  $('#js-save form').submit(function() {
+    var $this = $(this);
+    var $block = $this.closest('#js-save');
+    var action = $this.data('action');
+    var data_form = $this.serialize();
+    // $.ajax({
+    //   url: action,
+    //   type: "POST",
+    //   data: data_form,
+    //   success: function(data){
+    //     $block.html(data.content);
+    //     $('.col-r disabled').removeClass('disabled');
+    //     $('#js-github').attr('href', data.githab);
+    //   }
+    // });
+    $('.col-r .disabled').removeClass('disabled');
+    $('#js-github').attr({'href': 'data.githab'});
+    return false;
+  });
 });
