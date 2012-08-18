f09250d2d0d8fe752c76c7b7710266aecbf339b5
Denis Veselov <veselov@trilan.ru>
2012-08-18 16:01:21 +0400

    add development github callback url

--- project/settings/development.py
+++ project/settings/development.py
@@ -27,6 +27,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 INTERNAL_IPS = ('127.0.0.1',)
 GOOGLE_CALLBACK_URL = 'http://localhost:8000/blogs/oauth2callback'
+GITHUB_CALLBACK_URL = 'http://localhost:8000/repositories/oauth2callback/'
 
 INSTALLED_APPS += ('devserver',)
 DEVSERVER_MODULES = []
