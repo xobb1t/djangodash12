62875e7cba5464e61713adb083d864f5971c7dc4
Denis Veselov <veselov@trilan.ru>
2012-08-18 12:55:25 +0400

    change aouth callback url for github

--- project/settings/common.py
+++ project/settings/common.py
@@ -157,7 +157,7 @@ GITHUB_SCOPES = ('user', 'public_repo',)
 GITHUB_AUTH_HOST = 'https://github.com'
 GITHUB_AUTH_PATH = '/login/oauth/authorize'
 GITHUB_API_HOST = 'https://api.github.com'
-GITHUB_CALLBACK_URL = 'http://lz367.o1.gondor.io/repositories/github_auth/'
+GITHUB_CALLBACK_URL = 'http://lz367.o1.gondor.io/repositories/oauth2callback/'
 
 
 GOOGLE_ID = os.environ.get('GOOGLE_ID', '')
