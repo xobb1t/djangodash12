a938dd3caafad66aad0e13bd3fc21d15f0df40fa
Dima Kukushkin <dima@kukushkin.me>
2012-08-18 10:37:20 +0400

    Add apps dir for project applications

---
+++ project/settings/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+apps_root = os.path.join(
+    os.path.dirname(__file__),
+    os.parname, os.parname, 
+    'apps'
+)
+apps_root = os.path.normpath(os.path.abspath(apps_root))
+if apps_root not in sys.path:
+    sys.path.insert(0, apps_root)
