32ec933641e69c255cc636f331f72f3cbdd2389d
Denis Veselov <veselov@trilan.ru>
2012-08-18 14:38:16 +0400

    add login test page for github

--- apps/repositories/urls.py
+++ apps/repositories/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from .views import oauth2callback
 
@@ -6,5 +7,12 @@ from .views import oauth2callback
 urlpatterns = patterns(
     '',
     url(r'^oauth2callback/$', oauth2callback,
-        name='repositories_oauth2callback'),
+        name='repositories_oauth2callback'
+    ),
+    url(r'^login/$', 
+        TemplateView.as_view(
+            template_name='repositories/login.html'
+        ), 
+        name='repositories_login'
+    ),
 )
