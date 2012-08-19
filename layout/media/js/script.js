4af02b0c0c3f0b37db2488f4d934ef41b03a7651
Alexander Aslanov <trolev@gmail.com>
2012-08-19 10:20:19 +0400

    Remove bad files

--- layout/media/js/script.js
+++ layout/media/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       $('li:first', block).hide();
     }
   });
-  $('#js-sblog form').submit(function() {
+  $('#js-sblog form').submit(function(){
     var $this = $(this);
     var $block = $this.closest('#js-sblog');
     var action = $this.data('action');
@@ -29,4 +29,25 @@ $(document).ready(function(){
     $('#js-github').attr({'data-content': 'Sign in via OAuth2 protocol'});
     return false;
   });
+
+  $('#js-repo form').submit(function(){
+    var $this = $(this);
+    var $block = $this.closest('#js-repo');
+    var action = $this.data('action');
+    var data_form = $this.serialize();
+    // $.ajax({
+    //   url: action,
+    //   type: "POST",
+    //   data: data_form,
+    //   success: function(data){
+    //     $block.html(data.content);
+    //     $('#js-tr .disabled, #js-br .disabled').removeClass('disabled');
+    //     $('#js-github').attr('href', data.githab);
+    //     $('#js-github').attr({'data-content': 'Sign in via OAuth2 protocol'});
+    //   }
+    // });
+    alert(3);
+    return false;
+  });
+
 });
