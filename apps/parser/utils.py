1aa0644ff200e82a87da08e8d636acb91bb99052
Dima Kukushkin <dima@kukushkin.me>
2012-08-20 02:47:43 +0400

    Still copying dirs

--- apps/parser/utils.py
+++ apps/parser/utils.py
@@ -70,7 +70,7 @@ def create_pelican_instance(process):
     except OSError:
         pass
     pelican_src = os.path.join(settings.PACKAGE_ROOT, 'pelican')
-    Popen(['cp', '-r', '*', process.path], cwd=pelican_src).wait()
+    Popen(['cp', '-r', '.', process.path], cwd=pelican_src).wait()
 
 
 def convert_blog_posts(process, posts):
